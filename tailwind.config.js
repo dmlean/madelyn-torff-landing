56b51e389b70cd266ee349f314144d7eb0280031
dmlean <dmlean@protonmail.com>
2026-02-23 04:16:49 +0100

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./input.css"],
+  theme: {
+    fontFamily: {
+      'nunito': ['Nunito'],
+      'playfairdisplay': ['Playfair Display']
+    },
+    extend: {},
+  },
+  plugins: [],
+};
